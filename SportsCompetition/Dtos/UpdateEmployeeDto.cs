798f86624ea96b99ee52b4721132bec2aa8e47b6
Андрей Марчук <ANDREY.MAR4UK2011@YANDEX.RU>
2023-04-09 23:25:43 +0300

    Added_Roles

--- SportsCompetition/Dtos/UpdateEmployeeDto.cs
+++ SportsCompetition/Dtos/UpdateEmployeeDto.cs
@@ -2,9 +2,10 @@
 {
     public class UpdateEmployeeDto
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string? Specialization { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
--- SportsCompetition/Models/Employee.cs
+++ SportsCompetition/Models/Employee.cs
@@ -2,12 +2,13 @@
 {
     public class Employee
     {
+        public readonly string[] Roles = { "Administrator", "Secretary", "Assistant", "Judge" };
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public Role Role { get; set; }
+        public string Role { get; set; } 
 
         [Newtonsoft.Json.JsonIgnore]
         [System.Text.Json.Serialization.JsonIgnore]
